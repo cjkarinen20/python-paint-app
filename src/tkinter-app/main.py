5ba29b6abd4962cbd385523dad3fdfa48df370ee
cjkarinen20 <cjkarinen20@gmail.com>
2026-04-29 17:13:53 -0400

    Draw Grid Block
    -Wrote code to display one block of what will
    become the drawing grid.

--- src/tkinter-app/main.py
+++ src/tkinter-app/main.py
@@ -5,6 +5,19 @@ class PixelApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Pixel Art")
+    
+        cell_length = 50
+        grid_length = 20
+        grid_height = 10
+        
+        self.drawing_grid = Canvas(self.root)
+        self.drawing_grid.grid(column = 0, row = 0, sticky = (N, E, S, W))
+        
+        self.cells = []
+        cell = Frame(self.drawing_grid, width = cell_length, height = cell_length, bg = "white", highlightbackground = "black", highlightcolor = "black", highlightthickness = 1)
+        cell.grid(column = 0, row = 0)
+        self.cells.append(cell)
+        
         
 root = Tk()
 PixelApp(root)
