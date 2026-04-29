e83cb8c5006dad2289b51fe2862ee7b7a28e87b1
cjkarinen20 <cjkarinen20@gmail.com>
2026-04-29 17:52:28 -0400

    Control Bar Button Styling and Padding
    -Adding Styling and Padding to the Control Bar
    buttons.

--- src/tkinter-app/main.py
+++ src/tkinter-app/main.py
@@ -20,28 +20,44 @@ class PixelApp:
                 cell.grid(column = j, row = i)
                 cell.bind('<Button-1>', self.tap_cell)
                 self.cells.append(cell)
+        
+        #-----Control-Panel-----
                 
-        control_frame = Frame(self.root, height = cell_length)
-        control_frame.grid(column = 0, row = 1, sticky = (N, E, S, W))
+        # Creates the control bar frame on the bottom.
+        control_bar = Frame(self.root, height = cell_length)
+        control_bar.grid(column = 0, row = 1, sticky = (N, E, S, W))
         
-        new_button = Button(control_frame, text = "New")
-        new_button.grid(column = 0, row = 0)
+        # Button to create a new drawing.
+        new_button = Button(control_bar, text = "New")
+        new_button.grid(column = 0, row = 0, columnspan = 2, sticky = (N, E, S, W), padx = 5, pady = 5)
         
-        save_button = Button(control_frame, text = "Save")
-        save_button.grid(column = 2, row = 0)
+        # Button to save the current drawing.
+        save_button = Button(control_bar, text = "Save")
+        save_button.grid(column = 2, row = 0, columnspan = 2, sticky = (N, E, S, W), padx = 5, pady = 5)
         
-        pen_button = Button(control_frame, text = "Pen")
-        pen_button.grid(column = 8, row = 0)
+        # Button to switch to the pen tool.
+        pen_button = Button(control_bar, text = "Pen")
+        pen_button.grid(column = 8, row = 0, columnspan = 2, sticky = (N, E, S, W), padx = 5, pady = 5)
         
-        erase_button = Button(control_frame, text = "Erase")
-        erase_button.grid(column = 10, row = 0)
+        # Button to switch to the erase tool.
+        erase_button = Button(control_bar, text = "Erase")
+        erase_button.grid(column = 10, row = 0, columnspan = 2, sticky = (N, E, S, W), padx = 5, pady = 5)
         
-        selected_color_box = Frame(control_frame, borderwidth = 2, relief = "raised", bg = "white")
-        selected_color_box.grid(column = 15, row = 0)
+        # Box displaying the currently selected color.
+        selected_color_box = Frame(control_bar, borderwidth = 2, relief = "raised", bg = "white")
+        selected_color_box.grid(column = 15, row = 0, columnspan = 2, sticky = (N, E, S, W), padx = 7, pady = 7)
         
-        pick_color_button = Button(control_frame, text = "Pick Color")
-        pick_color_button.grid(column = 17, row = 0)
+        # Button to pick a new color.
+        pick_color_button = Button(control_bar, text = "Pick Color")
+        pick_color_button.grid(column = 17, row = 0, columnspan = 3, sticky = (N, E, S, W), padx = 5, pady = 5)
         
+        cols, rows = control_bar.grid_size()
+        for col in range(cols):
+            control_bar.columnconfigure(col, minsize = cell_length)
+        control_bar.rowconfigure(0, minsize = cell_length)
+        
+        
+    # Event method for when a cell is pressed.
     def tap_cell(self, event):
         print("Cell Tapped")
         
