6f2207cd87aea6df9149fb05aaf8028c1eec5ecc
cjkarinen20 <cjkarinen20@gmail.com>
2026-04-29 17:19:32 -0400

    Render Full Drawing Grid
    -The entire grid now displays.
    -A basic event triggers when a cell is tapped.

--- src/tkinter-app/main.py
+++ src/tkinter-app/main.py
@@ -7,17 +7,22 @@ class PixelApp:
         self.root.title("Pixel Art")
     
         cell_length = 50
-        grid_length = 20
+        grid_width = 20
         grid_height = 10
         
         self.drawing_grid = Canvas(self.root)
         self.drawing_grid.grid(column = 0, row = 0, sticky = (N, E, S, W))
         
         self.cells = []
-        cell = Frame(self.drawing_grid, width = cell_length, height = cell_length, bg = "white", highlightbackground = "black", highlightcolor = "black", highlightthickness = 1)
-        cell.grid(column = 0, row = 0)
-        self.cells.append(cell)
+        for i in range(0, grid_height):
+            for j in range(0, grid_width):
+                cell = Frame(self.drawing_grid, width = cell_length, height = cell_length, bg = "white", highlightbackground = "black", highlightcolor = "black", highlightthickness = 1)
+                cell.grid(column = j, row = i)
+                cell.bind('<Button-1>', self.tap_cell)
+                self.cells.append(cell)
         
+    def tap_cell(self, event):
+        print("Cell Tapped")
         
 root = Tk()
 PixelApp(root)
